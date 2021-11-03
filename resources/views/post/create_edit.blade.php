6b110dbe349d43c310b975aaef5ee3f5f186819e
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-03 13:55:38 -0300

    Brincando com layout dos posts

--- resources/views/post/create_edit.blade.php
+++ resources/views/post/create_edit.blade.php
@@ -3,3 +3,20 @@
 @section('content')
 
 @endsection
+
+
+<!--<div class="container">
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <img class="img-fluid" src="https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg" alt="notebook">
+            </div>
+            <div class="col-md-5 text-center">
+                <h1>Lorem Ipsulum</h1>
+                <p class="mt-4 text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi architecto consequatur consequuntur culpa doloremque harum labore libero obcaecati perferendis praesentium repellat repellendus sit, tenetur. Delectus doloribus labore laboriosam quidem velit.</p>
+                <a href="#" class="btn btn-outline-info">Saiba mais</a>
+            </div>
+            <div class="col-md-3" style="border: 1px solid black;">
+                <p>Propaganda</p>
+            </div>
+        </div>
+    </div> -->
