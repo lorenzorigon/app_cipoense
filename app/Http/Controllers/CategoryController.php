55e4d6710c13cc50033edf2b75426ca51055c20a
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-07 11:27:02 -0300

    Adicionando categoria

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function show($id){
+
+    }
+    
+    public function create($id){
+
+    }
+
+    public function store(Request $request, $id){
+
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
