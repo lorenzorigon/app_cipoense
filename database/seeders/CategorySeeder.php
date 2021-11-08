885bcf51d8bcf1a9e07ac440111290d1009cbf07
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-08 09:19:06 -0300

    Criando seeder de categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,14 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $names = ['Política', 'Religião', 'Policial', 'Filmes', 'Tragédia', 'Acidente', 'Ação Social', 'Esportes', 'Prefeitura'];
+
+        foreach ($names as $name){
+            Category::insert(
+                ['name' => $name],
+            );
+        }
+
+
     }
 }
