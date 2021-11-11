8684ee47dc91e18f66db4199018374c3a5b5f43d
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-11 10:37:16 -0300

    Criando o page controller para o site

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function about()
+    {
+
+    }
+
+    public function contact()
+    {
+
+    }
+
+    public function schedule()
+    {
+
+    }
+
+    public function announcers()
+    {
+        
+    }
+}
